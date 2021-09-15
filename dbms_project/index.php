28a521e97f39512681aae7e225a74f21aea16c63
srinivasvasa <67817715+srinivasvasa@users.noreply.github.com>
2021-09-15 17:24:43 +0530

    first commit without full home page

---
+++ dbms_project/index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Kaisei+Tokumin:wght@700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+	<title>LEARNIT</title>
+</head>
+<body style="background-color: #8FC1D4;">
+	<!-- NAV BAR -->
+	<nav class="navbar navbar-expand-sm navbar-light fixed-top" style="background-color: #8FC1D4;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">LEARN IT</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav custom-nav">
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Courses</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Payment Status</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">My Profile</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Logout</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Signup</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Feedback</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- bg video -->
+<div class="container-fluid remove-vid-marg">
+  <div class="video-parent">
+    <video playsinline autoplay muted loop>
+      <source src="viedo/bgvideo.mp4">
+    </video>
+    <div class="vid-overlay"></div>
+  </div>
+  <div class="vid-content">
+    <h1 class="my-content">Welcome To Learn It</h1>
+    <a href="#" class="btn btn-danger start">Get Started</a>
+  </div>
+</div>
+
+<!-- text banner -->
+<div class="container-fluid txt-banner bg-danger">
+  <div class="row bottom-banner">
+    <div class="col-sm">
+      <h5><i class="fas fa-book-open mr-3"></i> 50+ Online Courses</h5>
+    </div> 
+    <div class="col-sm">
+      <h5><i class="fas fa-users mr-3"></i> Expert Instructors</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-keyboard mr-3"></i> Lifetime Access</h5>
+    </div>
+  </div>
+</div>
+
+<!-- popular courses 1-->
+<div class="container">
+  <div>
+    <div class="row">
+      <div class="col-12 center">
+        <h1 style="padding-top: 20px;">Popular Courses</h1>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="text-center m-2">
+      <a href="#" class="btn btn-danger btn-sm" style="margin-top: 20px; margin-bottom: 20px;">View All Courses</a>
+    </div>
+  </div>
+</div>
+
+<!-- contact us form-->
+<div class="container">
+  <div class="row">
+    <div class="col-12 center">
+      <h1 style="padding-bottom: 20px;">Contact Us</h1>
+    </div>
+  </div>
+  <div class="col">
+    <form action="" method="post">
+      <input type="text" name="name" class="form-control" placeholder="Name"><br>
+      <input type="text" name="Course_Name" class="form-control" placeholder="Course name"><br>
+      <input type="text" name="Email" class="form-control" placeholder="Email"><br>
+      <textarea type="text" name="Message" class="form-control" placeholder="Issue" style="height: 150px;"></textarea><br>
+      <div class="row">
+        <div class="col-12 center">
+          <input type="submit" class="btn btn-primary" value="Send" name="submit">
+          <br><br>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+<!-- student feedback -->
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <h1 class="text-center" style="padding-top: 1em; padding-bottom: 1em;">FEEDBACKS</h1>
+  <div class="carousel-indicators">
+
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner" style="padding-left: 2em; padding-right: 2em;">
+    <div class="carousel-item active">
+      <div class="row">
+        <div class="col-4">
+          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+            <div class="card">
+              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+              <div class="card-body">
+                <h5 class="card-title">Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-4">
+          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+            <div class="card">
+              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+              <div class="card-body">
+                <h5 class="card-title">Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-4">
+          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+            <div class="card">
+              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+              <div class="card-body">
+                <h5 class="card-title">NAME</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="row">
+        <div class="col-4">
+          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+            <div class="card">
+              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+              <div class="card-body">
+                <h5 class="card-title">Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-4">
+          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+            <div class="card">
+              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+              <div class="card-body">
+                <h5 class="card-title">Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-4">
+          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+            <div class="card">
+              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+              <div class="card-body">
+                <h5 class="card-title">NAME</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+          </a>
+        </div>      
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+  
+<script type="text/javascript", src="js/jquery.min.js"></script>
+<script type="text/javascript", src="js/popper.min.js"></script>
+<script type="text/javascript", src="js/bootstrap.min.js"></script>
+<script type="text/javascript", src="js/all.min.js"></script>
+
+</body>
+</html>
