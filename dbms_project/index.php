feddacb8b5ffa0605e9eea381d6ee38434c04c7f
srinivasvasa <67817715+srinivasvasa@users.noreply.github.com>
2021-09-16 13:41:45 +0530

    full home page

--- dbms_project/index.php
+++ dbms_project/index.php
@@ -9,6 +9,12 @@
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Kaisei+Tokumin:wght@700&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+    integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+    integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+    crossorigin="anonymous" />
 
 
 	<title>LEARNIT</title>
@@ -240,115 +246,136 @@
 </div>
 
 <!-- student feedback -->
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <h1 class="text-center" style="padding-top: 1em; padding-bottom: 1em;">FEEDBACKS</h1>
-  <div class="carousel-indicators">
+<h1 style="padding-top: 1em; padding-bottom: 1em; text-align: center;">OUR STUDENT FEEDBACKS</h1>
+<div class="owl-carousel owl-theme" style="padding-right: 1.5em; padding-left:1.5em ; text-align: center;">
 
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner" style="padding-left: 2em; padding-right: 2em;">
-    <div class="carousel-item active">
-      <div class="row">
-        <div class="col-4">
-          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
-            <div class="card">
-              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
-              <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-              </div>
-            </div>
-          </a>
+    <div class="item">
+      <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">STUDENT NAME</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
         </div>
 
-        <div class="col-4">
-          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
-            <div class="card">
-              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
-              <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-4">
-          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
-            <div class="card">
-              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
-              <div class="card-body">
-                <h5 class="card-title">NAME</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
     </div>
-    <div class="carousel-item">
-      <div class="row">
-        <div class="col-4">
-          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
-            <div class="card">
-              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
-              <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-              </div>
-            </div>
-          </a>
+    <div class="item">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">STUDENT NAME</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
         </div>
-
-        <div class="col-4">
-          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
-            <div class="card">
-              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
-              <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-              </div>
-            </div>
-          </a>
+    </div>
+    <div class="item">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">STUDENT NAME</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
         </div>
-
-        <div class="col-4">
-          <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
-            <div class="card">
-              <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
-              <div class="card-body">
-                <h5 class="card-title">NAME</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-              </div>
-            </div>
-          </a>
-        </div>      
+    </div>
+    <div class="item">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">STUDENT NAME</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">STUDENT NAME</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-danger">
+  <div class="row text-white text-center p-1">
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+    </div>
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+    </div>
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+    </div>
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-instagram"></i> Instagram</a>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
 
+<div class="container-fluid p-4" style="background-color: #E9ECEF">
+  <div class="container" style="background-color: #E9ECEF">
+    <div class="row text-center">
+      <div class="col-sm">
+        <h5>About Us</h5>
+        <p>iSchool provides universal access to the world's best education, partnering with top universities and organizations to offer courses online.</p>
+      </div>
+      <div class="col-sm">
+        <h5>Category</h5>
+        <a class="text-dark" href="#">Web Development</a><br />
+        <a class="text-dark" href="#">Web Designing</a><br />
+        <a class="text-dark" href="#">Android App Dev</a><br />
+        <a class="text-dark" href="#">iOS Development</a><br />
+        <a class="text-dark" href="#">Data Analysis</a><br />
+      </div>
+      <div class="col-sm">
+        <h5>Contact Us</h5>
+        <p>LEARNIT Pvt Ltd <br> HYDERABAD,TELANGANA <br> Ph. 000000000 </p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Start Footer -->
+
+<footer class="container-fluid bg-dark text-center p-2"> <small class="text-white">Copyright &copy; 2025 || LEARNIT.com</small>
+
+</footer>
 
   
-<script type="text/javascript", src="js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+<!-- Owl Carousel -->
 <script type="text/javascript", src="js/popper.min.js"></script>
 <script type="text/javascript", src="js/bootstrap.min.js"></script>
 <script type="text/javascript", src="js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<!-- custom JS code after importing jquery and owl -->
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".owl-carousel").owlCarousel();
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+</script>
 
 </body>
 </html>
